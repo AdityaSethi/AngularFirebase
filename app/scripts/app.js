24420495b0df7d514d3c7b0061ae2bfced1d68de
Aditya Sethi <aditya_sethi@yahoo.co.in>
2014-12-02 21:26:21 +0530

    creating frontend base

---
+++ app/scripts/app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularFirebaseApp
+ * @description
+ * # angularFirebaseApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularFirebaseApp', [
+    'ui.router',
+    'news.post.controllers',
+  ])
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('posts', {
+        url: 'posts',
+        templateUrl: 'scripts/post/post.html',
+        controller: 'PostCtrl'
+      })
+  })
+  .run(function ($state) {
+    $state.go('posts');
+  });
